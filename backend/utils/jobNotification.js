87c38dc15ebcab564b96f5ee4adb442697fdbb0e
ali raza <ar6680519@gmail.com>
2026-04-21 13:55:47 +0500

    Add files via upload

---
+++ backend/utils/jobNotification.js
@@ -0,0 +1,29 @@
+import User from "../models/userSchema.js";
+import { sendEmail } from "./sendEmail.js";
+
+export const triggerJobNotification = async (job) => {
+    try {
+        const users = await User.findAll();
+
+        const matchedUsers = users.filter(user =>
+            [
+                user.niches?.firstNiche,
+                user.niches?.secondNiche,
+                user.niches?.thirdNiche
+            ].includes(job.jobNiche)
+        );
+
+        // IMPORTANT: non-blocking email sending
+        for (const user of matchedUsers) {
+            sendEmail({
+                email: user.email,
+                subject: `New Job Alert: ${job.title}`,
+                message: `Hi ${user.name}, new job posted: ${job.title}`
+            }).catch(err => console.log("Email failed:", err.message));
+        }
+
+    };
+
+} catch (err) {
+    console.log("Notification error:", err.message);
+};
