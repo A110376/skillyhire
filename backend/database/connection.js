ef5f15acf3073a587847cf6f4dc5ed9458aeb8be
ali raza <ar6680519@gmail.com>
2026-04-21 12:43:55 +0500

    Add pg module for PostgreSQL connection

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import pg from "pg";
 
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
+  dialectModule: pg,
   logging: false,
   dialectOptions: {
     ssl: {
