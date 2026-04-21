d08cb2ba5ad33f48268d30ad722635ec0bd6acb3
ali raza <ar6680519@gmail.com>
2026-04-21 13:57:13 +0500

    Refactor postJob to trigger job notifications
    Refactored job posting logic to trigger notifications after job creation.

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -2,8 +2,7 @@ import { Op } from "sequelize";
 import Job from "../models/jobSchema.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/error.js";
-import User from "../models/userSchema.js";
-import { sendEmail } from "../utils/sendEmail.js";
+import { triggerJobNotification } from "../utils/jobNotification.js";
 
 export const postJob = catchAsyncErrors(async (req, res, next) => {
   const {
@@ -79,31 +78,8 @@ export const postJob = catchAsyncErrors(async (req, res, next) => {
     jobNiche: jobNiche.trim(),
     postedBy,
   });
-  const users = await User.findAll();
-
-const matchedUsers = users.filter(user =>
-  [user.niches?.firstNiche, user.niches?.secondNiche, user.niches?.thirdNiche]
-    .includes(job.jobNiche)
-);
-
-for (const user of matchedUsers) {
-  try {
-    await sendEmail({
-      email: user.email,
-      subject: `New Job Alert: ${job.title}`,
-      message: `Hi ${user.name},
-
-New job posted:
-- ${job.title}
-- ${job.companyName}
-- ${job.location}
-
-Apply now!`
-    });
-  } catch (err) {
-    console.log("Email failed:", err.message);
-  }
-}
+  
+await triggerJobNotification(job);
 
   res.status(201).json({
     success: true,
